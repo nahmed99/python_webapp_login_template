0ce287e2010fa97840c3862ae0da1ad3e6b527ef
nahmed99 <nahmed99>
2021-04-05 21:26:41 +0100

    set up Flask SQLAlchemy

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
    app = Flask(__name__)
    app.config['SECRET_KEY'] = 'jur821jd39dsfgh sdfj' # In PRODUCTION, this needs to be kept secret!!! At the moment it is just some random (secret!) value.
+
+   # The database will be stored in the same location as the __init__.py file:
+   app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+   db.init_app(app) # the app that we will use with this database
 
    # import blueprints
    from .views import views
